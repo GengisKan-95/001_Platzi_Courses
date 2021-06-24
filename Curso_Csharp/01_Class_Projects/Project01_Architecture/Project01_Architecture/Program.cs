91e9a3e536efcebf80dfbf95f37913b7ad33278d
CamiloHamon13 <camih1338@hotmail.com>
2021-06-24 13:03:53 -0500

    Se agrega un nuevo proyecto de C sharp

---
+++ Curso_Csharp/01_Class_Projects/Project01_Architecture/Project01_Architecture/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project01_Architecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
